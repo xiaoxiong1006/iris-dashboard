9fb5e9bddd521281d3fac4e0ce361959dcbc4a17
xiaoxiong <xiaoxiong@192.168.1.101>
2023-12-17 14:47:34 +0800

    add petal data api

--- app.py
+++ app.py
@@ -6,15 +6,15 @@ app = Flask(__name__)
 # 开启调试模式
 app.config["DEBUG"] = True
 
+# 读取数据集
+iris = pd.read_csv("data/iris.csv")
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/iris/species/count')
 def iris_species_count():
-    # 读取数据集
-    iris = pd.read_csv("data/iris.csv")
-
     # 查看种类的分布情况
     species_counts = iris.Species.value_counts()
 
@@ -23,3 +23,32 @@ def iris_species_count():
 
     # 返回JSON数据
     return jsonify(echarts_data)
+
+@app.route('/iris/species/petal')
+def iris_species_petal():
+    # 获取山鸢尾（Iris-setosa）的记录
+    Iris_setosa = iris[iris.Species=='Iris-setosa']
+
+    # 获取杂色鸢尾（Iris Versicolour）的记录
+    Iris_versicolor = iris[iris.Species=='Iris-versicolor']
+
+    # 获取弗吉尼亚鸢尾 Iris Virginica）的记录
+    Iris_virginica = iris[iris.Species=='Iris-virginica']
+
+    # 构造返回的 JSON 数据
+    data_to_return = [
+        {
+            "name": "Iris-setosa",
+            "data": Iris_setosa[['PetalLengthCm', 'PetalWidthCm']].values.tolist()
+        },
+        {
+            "name": "Iris-versicolor",
+            "data": Iris_versicolor[['PetalLengthCm', 'PetalWidthCm']].values.tolist()
+        },
+        {
+            "name": "Iris-virginica",
+            "data": Iris_virginica[['PetalLengthCm', 'PetalWidthCm']].values.tolist()
+        }
+    ]
+
+    return jsonify(data_to_return)
